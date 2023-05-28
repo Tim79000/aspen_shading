f0f58421655383c0ea547e5ffa03e75cd065dd14
Tim7900 <100736284+Tim79000@users.noreply.github.com>
2023-05-28 13:25:35 -0800

    First Commit

---
+++ init.lua
@@ -0,0 +1,8 @@
+
+-- Wow, this is it?
+
+
+minetest.override_item("default:aspen_tree", {
+	tiles = {"default_aspen_tree_top.png^aspen_shading_top.png", "default_aspen_tree_top.png^aspen_shading_top.png",
+		"default_aspen_tree.png^aspen_shading_side.png"}
+})
